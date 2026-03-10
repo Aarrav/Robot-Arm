bb2eddb4ccfdbbf02b6e9bbeb18f7cad55c77fe4
Aarrav Anil <aarravanil@gmail.com>
2026-03-10 01:03:14 +0100

    Encoder read try

--- System identification base/src/main.cpp
+++ System identification base/src/main.cpp
@@ -9,9 +9,15 @@
 Encoder baseEnc(baseMotorEncoderA, baseMotorEncoderB);
 
 long oldPositionBase;
-unsigned long oldTimeBase;
+unsigned long oldTimeBase, startTime;
 
-const float countsPerRot = 20100.0;   // VERIFY THIS
+// Driving gear = 20 teeth
+// Driven gear = 30 teeth
+
+// Encoder counts per revolution of the motor shaft = 13384
+// Encoder counts per revolution of the output shaft = 13384 * (30/20) = 20076
+
+const float countsPerRot = 13384.0; 
 
 void setup() {
   pinMode(baseMotorPWM1, OUTPUT);
@@ -21,41 +27,46 @@ void setup() {
 
   oldPositionBase = baseEnc.read();
   oldTimeBase = micros();
+  delay(2000); 
+  startTime = millis();
+
 }
 
 void loop() {
 
-  // Motor direction switching
-  unsigned long t = millis() % 1500;
-  if (t < 750) {
+  if (millis() - startTime < 1000) {
     analogWrite(baseMotorPWM1, 0);
-    analogWrite(baseMotorPWM2, 40);
+    analogWrite(baseMotorPWM2, 20);
   } else {
-    analogWrite(baseMotorPWM1, 40);
+    analogWrite(baseMotorPWM1, 0);
     analogWrite(baseMotorPWM2, 0);
   }
 
   // Velocity update every 10ms
   static unsigned long lastUpdate = 0;
-  if (millis() - lastUpdate >= 1) {
+  if (micros() - lastUpdate >= 100000) {
 
-    long newPosition = baseEnc.read();
-    unsigned long newTime = micros();
+    long newPositionBase = baseEnc.read();
+    unsigned long newTimeBase = micros();
 
-    float deltaCounts = newPosition - oldPositionBase;
-    float deltaTime = (newTime - oldTimeBase) / 1000000.0;
+    float deltaCounts = newPositionBase - oldPositionBase;
+    float deltaTime = (newTimeBase - oldTimeBase) / 100000.0;
 
-    float positionDeg = (newPosition / countsPerRot) * 360.0;
+    float positionDeg = (newPositionBase / countsPerRot) * 360.0;
     float velocityDeg = (deltaCounts / countsPerRot) * 360.0 / deltaTime;
 
-    Serial.print("Position: ");
-    Serial.print(positionDeg);
-    Serial.print(" deg | Velocity: ");
-    Serial.print(velocityDeg);
-    Serial.println(" deg/s");
+    // Serial.print("Position: ");
+    // Serial.print(positionDeg);
+    // Serial.print(" deg | Velocity: ");
+    // Serial.print(velocityDeg);
+    // Serial.println(" deg/s");
 
-    oldPositionBase = newPosition;
-    oldTimeBase = newTime;
+    Serial.print(newPositionBase);
+    Serial.print(" , ");
+    Serial.println(oldPositionBase);
+
+    oldPositionBase = newPositionBase;
+    oldTimeBase = newTimeBase;
     lastUpdate = millis();
   }
 }
